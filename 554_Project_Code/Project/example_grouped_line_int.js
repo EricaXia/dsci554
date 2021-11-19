fd510374904602ce022b93a3e8ec82d226bb131a
Erica Xia <ericaxia3@gmail.com>
2021-11-19 09:38:44 -0800

    change to const

--- 554_Project_Code/Project/example_grouped_line_int.js
+++ 554_Project_Code/Project/example_grouped_line_int.js
@@ -1,18 +1,18 @@
-var margin = { top: 50, right: 50, bottom: 50, left: 50 }
-var h = 500 - margin.top - margin.bottom
-var w = 700 - margin.left - margin.right
+const margin = { top: 50, right: 50, bottom: 50, left: 50 }
+const h = 500 - margin.top - margin.bottom
+const w = 700 - margin.left - margin.right
 
-var formatDecimal = d3.format('.0')
+const formatDecimal = d3.format('.0')
 
 const data_url = 'Project/d3layout_data/ex_data.csv'
 d3.csv(data_url).then((data) => {
   // console.log(data)
 
   // Scales
-  var x = d3.scaleLinear()
+  const x = d3.scaleLinear()
     .domain([2000, 2005])
     .range([0, w])
-  var y = d3.scaleLinear()
+  const y = d3.scaleLinear()
     .domain([
       d3.min([0, d3.min(data, function (d) { return d.wvalue })]),
       d3.max([0, d3.max(data, function (d) { return d.wvalue })])
@@ -23,13 +23,13 @@ d3.csv(data_url).then((data) => {
 
 
   // Define the line
-  var valueLine = d3.line()
+  const valueLine = d3.line()
     .x(function (d) { return x(d.year); })
     .y(function (d) { return y(d.wvalue); })
   // .attr('fill', 'white')
 
-  // Create the svg canvas in the "d3block" div
-  var svg = d3.select("#d3block")
+  // Create svg
+  const svg = d3.select("#example-line-chart")
     .append("svg")
     .style("width", w + margin.left + margin.right + "px")
     .style("height", h + margin.top + margin.bottom + "px")
@@ -40,21 +40,22 @@ d3.csv(data_url).then((data) => {
     .attr("class", "svg");
 
   //nest variable aka GROUP
-  var nest = d3.groups(data,
+  const nest = d3.groups(data,
     d => d.state, d => d.lvalue)
 
   // X-axis
-  var xAxis = d3.axisBottom()
+  const xAxis = d3.axisBottom()
     .scale(x)
     .ticks(5)
+    .tickFormat(d3.format("d"))
 
   // Y-axis
-  var yAxis = d3.axisLeft()
+  const yAxis = d3.axisLeft()
     .scale(y)
   // .ticks(5)
 
   // Create a dropdown menu
-  var legisMenu = d3.select("#legisDropdown")
+  const legisMenu = d3.select("#legisDropdown")
 
   legisMenu
     .append("select")
@@ -66,9 +67,9 @@ d3.csv(data_url).then((data) => {
     .text(([key,]) => key)
 
   // Function to create the initial graph
-  var initialGraph = function (legis) {
-    var selectLegis = nest.filter(([key,]) => key == legis)
-    var selectLegisGroups = svg.selectAll(".legisGroups")
+  const initialGraph = function (legis) {
+    const selectLegis = nest.filter(([key,]) => key == legis)
+    const selectLegisGroups = svg.selectAll(".legisGroups")
       .data(selectLegis, function (d) {
         return d ? d.key : this.key;
       })
@@ -80,12 +81,13 @@ d3.csv(data_url).then((data) => {
     const color = d3.scaleOrdinal()
       .range(['#e41a1c', '#377eb8'])
 
-    var initialPath = selectLegisGroups.selectAll(".line")
+    const initialPath = selectLegisGroups.selectAll(".line")
       .data(([, values]) => values)
       .enter()
       .append("path")
       .attr("fill", "none") // ADDED
       .attr("stroke", function (d) { return color(d[0]) })
+      .attr("stroke-width", 3.0)
 
     initialPath
       .attr('d', (d) => valueLine(Array.from(d.values())[1]))
@@ -98,13 +100,13 @@ d3.csv(data_url).then((data) => {
 
 
   // Update the data
-  var updateGraph = function (legis) {
+  const updateGraph = function (legis) {
 
     // Filter the data to include only state of interest
-    var selectLegis = nest.filter(([key,]) => key == legis)
+    const selectLegis = nest.filter(([key,]) => key == legis)
 
     // Select all of the grouped elements and update the data
-    var selectLegisGroups = svg.selectAll(".legisGroups")
+    const selectLegisGroups = svg.selectAll(".legisGroups")
       .data(selectLegis)
 
     // Select all the lines and transition to new positions
@@ -118,7 +120,7 @@ d3.csv(data_url).then((data) => {
   // Run update function when dropdown selection changes
   legisMenu.on('change', function () {
     // Find which state was selected from the dropdown
-    var selectedLegis = d3.select(this)
+    const selectedLegis = d3.select(this)
       .select("select")
       .property("value")
     // Run update function with the selected state
